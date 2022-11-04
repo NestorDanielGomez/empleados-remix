5ac716cfea77514dbd60792940f31e0cb5d841b6
NestorDanielGomez <nestordanielgomez@gmail.com>
2022-11-04 19:29:54 -0300

    commit a otra branch

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Header() {
   return (
     <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">
-      Seguimiento Empleados {""}
+      Seguimiento Empleados. {""}
       <span className="text-indigo-400">OBRAS</span>
     </h1>
   );
